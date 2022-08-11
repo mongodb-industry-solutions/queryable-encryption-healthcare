ef935837443dc780e911caa49edd78793cf0f1ad
Jeff Needham <38022295+dataJeff@users.noreply.github.com>
2022-08-11 12:13:23 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# In Use Queryable Encryption Sample Application
+
+This repository contains a sample Node.js application detailing how to use Queryable Encryption using the AWS Key Management Service Provider.
+It contains an application that creates the data keys within a target MongoDB database, and an application that both inserts encrypted and un-encrypted fields within a sample record, and then reads that data, searching on the encrypted fields the MongoDB Queryable encryption features.
+
+## MongoDB Language Drivers:
+
+- Node.js
+
+
+## KMS Providers:
+
+- Amazon Web Services Key Management Service
+
+
+# About this Repo
+
+This repository is automatically generated. Please do not submit Pull Requests directly to this repository.
+# queryable-encryption-healthcare
---
+++ insert_encrypted_document.js
@@ -0,0 +1,138 @@
+const { MongoClient, Binary } = require("mongodb");
+
+// start-key-vault
+const eDB = "encryption";
+const eKV = "__keyVault";
+const keyVaultNamespace = `${eDB}.${eKV}`;
+// end-key-vault
+
+// start-kmsproviders
+const kmsProviders = {
+  aws: {
+    accessKeyId: "<Your AWS Access Key ID>",
+    secretAccessKey: "<Your AWS Secret Access Key>",
+  },
+};
+// end-kmsproviders
+
+async function run() {
+  // start-schema
+  const uri = "<Your AWS Secret Access Key>";
+  const unencryptedClient = new MongoClient(uri);
+  await unencryptedClient.connect();
+  const keyVaultClient = unencryptedClient.db(eDB).collection(eKV);
+
+  const dek1 = await keyVaultClient.findOne({ keyAltNames: "dataKey1" });
+  const dek2 = await keyVaultClient.findOne({ keyAltNames: "dataKey2" });
+  const dek3 = await keyVaultClient.findOne({ keyAltNames: "dataKey3" });
+  const dek4 = await keyVaultClient.findOne({ keyAltNames: "dataKey4" });
+  const dek5 = await keyVaultClient.findOne({ keyAltNames: "dataKey5" });
+  const dek6 = await keyVaultClient.findOne({ keyAltNames: "dataKey6" });
+
+  const secretDB = "healthRecords";
+  const secretCollection = "patients";
+
+  const encryptedFieldsMap = {
+    [`${secretDB}.${secretCollection}`]: {
+      fields: [
+        {
+          keyId: dek1._id,
+          path: "patientId",
+          bsonType: "int",
+          queries: { queryType: "equality" },
+        },
+        {
+          keyId: dek2._id,
+          path: "firstName",
+          bsonType: "string",
+          queries: { queryType: "equality" },
+        },
+        {
+        keyId: dek3._id,
+        path: "lastName",
+        bsonType: "string",
+        queries: { queryType: "equality" },
+      },
+        {
+          keyId: dek4._id,
+          path: "patientRecord.ssn",
+          bsonType: "string",
+          queries: { queryType: "equality" },
+        },
+        {
+          keyId: dek5._id,
+          path: "patientRecord.billing",
+          bsonType: "object",
+        },
+        {
+          keyId: dek6._id,
+          path: "address.street1",
+          bsonType: "string",
+        },
+      ],
+    },
+  };
+  // end-schema
+
+  // start-extra-options
+  const extraOptions = {
+    cryptSharedLibPath: "<path to FLE Shared Library Ex:/usr/local/bin/mongo_crypt_v1.dylib>",
+  };
+  // end-extra-options
+
+  // start-client
+  const encryptedClient = new MongoClient(uri, {
+    autoEncryption: {
+      keyVaultNamespace: keyVaultNamespace,
+      kmsProviders: kmsProviders,
+      extraOptions: extraOptions,
+      encryptedFieldsMap: encryptedFieldsMap,
+    },
+  });
+  await encryptedClient.connect();
+  // end-client
+  try {
+    const unencryptedColl = unencryptedClient
+      .db(secretDB)
+      .collection(secretCollection);
+
+    // start-insert
+    const encryptedColl = encryptedClient
+      .db(secretDB)
+      .collection(secretCollection);
+    await encryptedColl.insertOne({
+      firstName: "John",
+      lastName: "Doe",
+      patientId: 12345677,
+      address: {
+        street1: "157 Electric Ave.",
+        city: "Sioux Falls",
+        state: "SD"
+      },
+      patientRecord: {
+        ssn: "70906649",
+        billing: {
+          type: "Visa",
+          number: "4111111111111111",
+        },
+      },
+      medications: ["Lipitor"],
+    });
+    // end-insert
+    // start-find
+    console.log("Finding a document with regular (non-encrypted) client.");
+    console.log(await unencryptedColl.findOne({ firstName: /Jon/ }));
+    console.log(
+      "Finding a document with encrypted client, searching on an encrypted field"
+    );
+    console.log(
+      await encryptedColl.findOne({ "patientRecord.ssn": "70906649" })
+    );
+    // end-find
+  } finally {
+    await unencryptedClient.close();
+    await encryptedClient.close();
+  }
+}
+
+run().catch(console.dir);
